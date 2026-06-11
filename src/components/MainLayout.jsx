87c0686b90672683de49fbda492d43465a9af8fd
macbook <macbook@MacBook-Pro-cua-macbook.local>
2026-06-11 17:20:08 +0700

    chore: remove npm-cache from git tracking

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Header from './Header';
 
 export default function MainLayout() {
+  const location = useLocation();
+  const hideHeader = location.pathname.startsWith('/quality');
+
   return (
     <div className="app-layout">
       <Sidebar />
       <div className="app-main">
-        <Header />
+        {!hideHeader && <Header />}
         <div className="app-content">
           <Outlet />
         </div>
